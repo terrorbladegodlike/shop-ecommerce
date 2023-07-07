30273b8faa82dc0acce217868502b9d13ae5979a
terrorbladegodlike <ternovetchii.2002@mail.ru>
2023-07-07 11:34:09 +0300

    Added site context structure

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, {createContext, useState, useEffect} from 'react'
 
-const ProductContext = () => {
+// Create Context
+export const ProductContext = createContext()
+
+const ProductProvider = ({children}) => {
+
+  // Product State
+  const [products, setProducts] = useState([])
+
+  // Fetch Products
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('https://fakestoreapi.com/products')
+      const data = await response.json();
+      setProducts(data)
+    };
+    fetchProducts(); 
+  }, [])
+
   return (
-    <div>ProductContext</div>
+    <ProductContext.Provider value={{products}}>
+      {children}
+    </ProductContext.Provider>
   )
 }
 
-export default ProductContext
+export default ProductProvider
