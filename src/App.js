2de8b2bd33ed6816a9e4210d32772a4e392bc683
terrorbladegodlike <ternovetchii.2002@mail.ru>
2023-07-07 11:25:25 +0300

    Added router structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
+// Import React Router DOM
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+// Import Pages
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
+
+// Import Components
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
 const App = () => {
-  return <div className='overflow-hidden'>react app</div>;
+  return (
+    <div className='overlow-hidden'>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/product/:id' element={<ProductDetails />} />
+        </Routes>
+        <Sidebar />
+        <Footer />
+      </Router>
+    </div>
+  )
 };
 
 export default App;
