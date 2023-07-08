b8d25cff659641ca85b2a19ab957c8c687e681df
terrorbladegodlike <ternovetchii.2002@mail.ru>
2023-07-08 10:11:19 +0300

    Adding a 'Sidebar', animation of opening and closing the side menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+// Import SideBar Context
+import { SiderbarContext } from '../contexts/SidebarContext'
+// Import Icons
+import { BsBag } from 'react-icons/bs'
 
 const Header = () => {
+  const { isOpen, setIsOpen } = useContext(SiderbarContext)
   return (
-    <div>Header</div>
+    <header>
+      <div>Header</div>
+      <div onClick={() => setIsOpen(!isOpen)} className='cursor-pointer flex relative'>
+        <BsBag className='text-2xl' />
+      </div>
+    </header>
   )
 }
 
